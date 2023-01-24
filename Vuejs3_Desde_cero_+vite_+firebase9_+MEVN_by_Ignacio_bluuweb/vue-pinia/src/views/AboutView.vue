af9ac7d047c026eeb27e2980d26458b2994b2037
bruno <bruno.lorenzo.arroyo@gmail.com>
2023-01-24 10:57:53 +0000

    vue

---
+++ Vuejs3_Desde_cero_+vite_+firebase9_+MEVN_by_Ignacio_bluuweb/vue-pinia/src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useCounterStore } from "../stores/counter";
+
+const useCounter = useCounterStore();
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <h1>Home Counter: {{ useCounter.count }}</h1>
+  <h2>Double: {{ useCounter.doubleCount }}</h2>
+  <button @click="useCounter.increment">Increment</button>
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ Vuejs3_Desde_cero_+vite_+firebase9_+MEVN_by_Ignacio_bluuweb/vue-pinia/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+
+import { useCounterStore } from "../stores/counter";
+
+const useCounter = useCounterStore();
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1>Home Counter: {{ useCounter.count }}</h1>
+    <h2>Double: {{ useCounter.doubleCount }}</h2>
+    <button @click="useCounter.increment">Increment</button>
+  </main>
+</template>
